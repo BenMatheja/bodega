ed296a0e12dbe7fe0a994f8068ee7207f229e466
BenMatheja <bennimatheja@online.de>
2013-07-13 18:20:52 +0200

    edge repository added

---
+++ src/main/java/org/repositories/EdgeRepository.java
@@ -0,0 +1,8 @@
+package org.repositories;
+
+import org.model.Edge;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface EdgeRepository extends GraphRepository<Edge> {
+
+}
